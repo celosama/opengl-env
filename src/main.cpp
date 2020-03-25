883ea47cd20a1a896f211a0bc6b531cfa35dd10c
Marcelo Inhaquites <marcelo.inhaquites@gmail.com>
2020-03-25 19:07:06 -0300

    shaders: quad, indexed vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,17 @@ using namespace std;
 const GLchar* vertexShaderSrc =
 "#version 330 core\n"
 "layout (location = 0) in vec3 pos;"
-"layout (location = 1) in vec3 color;"
-"out vec3 vert_color;"
 "void main()"
 "{"
-"   vert_color = color;"
 "   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
 "}";
 
 const GLchar* framentShaderSrc =
 "#version 330 core\n"
-"in vec3 vert_color;"
 "out vec4 frag_color;"
 "void main()"
 "{"
-"   frag_color = vec4(vert_color, 1.0f);"
+"   frag_color = vec4(0.35f, 0.96f, 0.3f, 1.0f);"
 "}";
 
 int main() {
@@ -34,41 +30,34 @@ int main() {
   initializeGLEW();
   glfwSetKeyCallback(pWindow, onKey);
 
-  GLfloat vert_pos[] = {
-    // position
-     0.0f,  0.5f, 0.0f, // top
-     0.5f, -0.5f, 0.0f, // right
-    -0.5f, -0.5f, 0.0f, // left
+  GLfloat vertices[] = {
+    -0.5f,  0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
   };
 
-  GLfloat vert_color[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
+  GLuint indices[] = {
+    0, 1, 2, // tri 0
+    0, 2, 3  // tri 1
   };
 
-  GLuint vbo, vbo2, vao;
+  GLuint vbo, ibo, vao;
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vert_pos), vert_pos, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &vbo2);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vert_color), vert_color, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
   // position
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
   glEnableVertexAttribArray(0);
 
-  // color
-  glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  glEnableVertexAttribArray(1);
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   GLuint vs = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vs, 1, &vertexShaderSrc, NULL);
@@ -115,7 +104,7 @@ int main() {
 
     glBindVertexArray(vao);
     glUseProgram(shaderProgram);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     glfwSwapBuffers(pWindow);
