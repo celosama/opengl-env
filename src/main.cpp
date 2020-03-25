330f0d47d3503d2f63566273a28e684c5f8624f3
Marcelo Inhaquites <marcelo.inhaquites@gmail.com>
2020-03-25 01:57:29 -0300

    adds opengl, glew, glfw and os dependencies to linker

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-#include "test.h"
+
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 int main() {
-  std::cout << "Hello World" << hello();
+  if (!glfwInit()) {
+    std::cerr << "GLFW Failed to initialize.";
+    return 1;
+  }
+
   std::cin.get();
+
+  return 0;
 }
